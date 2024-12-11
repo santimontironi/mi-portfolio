fea1cb9a64a49f6febca0e02f86d39cd346a4576
santimontironi <santiimontironi@gmail.com>
2024-12-10 22:47:11 -0300

    actualizacion foto inicio

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,14 +1,21 @@
 import '../assets/css/aboutMe.css'
+import miFoto from '../assets/img/foto2.jpg'
 
 export const AboutMe = () => {
   return (
     <section id='aboutMe'>
+      <div className="container">
         <div className="foto">
-          <img src="../assets/img/foto2.jpg" alt="" />
+          <img src={miFoto} alt="" />
         </div>
         <div className="info">
           <h2>About Me</h2>
+          <p>Hi! I am Santiago Montironi, from Argentina, and I am 22 years old. As a developer passionate about technology, I have a web development startup called FullWeb, focused on creating high-perfomance and customized web applicactions.</p>
+          <p>My projects range from client management systems to interactive e-commerce plataforms, where I prorize optimization and clean code to ensure an excellent user experience.</p>
+          <p>I enjoy exploring new technologies and stay updated in tendences, always in search to improve and learn more with each project.</p>
+          <p>I am open to collaborating and learning together.</p>
         </div>
+      </div>
     </section>
   )
 }
