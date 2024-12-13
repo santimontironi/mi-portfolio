19ef124e2fca0facdca5af59753ddeec3ff153e9
santimontironi <santiimontironi@gmail.com>
2024-12-13 00:57:16 -0300

    Se empezo con la seccion Services

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Main } from "./pages/Main"
 import { AboutMe } from "./pages/AboutMe"
+import { Services } from "./pages/Services"
 
 const App = () => {
   return (
     <>
       <Main />
       <AboutMe />
+      <Services />
     </>
   )
 }
---
+++ src/pages/Services.jsx
@@ -0,0 +1,9 @@
+import '../assets/css/services.css'
+
+export const Services = () => {
+  return (
+    <div id="services">
+        <h1 className='titulo-services'>Services</h1>
+    </div>
+  )
+}
