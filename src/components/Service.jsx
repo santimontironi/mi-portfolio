bfe417743a48fb6dcb55056cd00479a3adec9d04
santimontironi <santiimontironi@gmail.com>
2024-12-20 17:37:57 -0300

    Segundo servicio agregado

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -4,7 +4,7 @@ export const Service = ({icono,titulo,descripcion}) => {
   return (
     <div className='servicio'>
         <p className='logo'>{icono}</p>
-        <h2>{titulo}</h2>
+        <h2 className='border-bottom border-solid border-light'>{titulo}</h2>
         <p>{descripcion}</p>
     </div>
   )
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -6,7 +6,8 @@ export const Services = () => {
     <section id="services">
       <h1 className='titulo-services'>My services</h1>
       <div className="contenedor-servicios">
-        <Service icono={<i className="bi bi-code-square"></i>} titulo='Frontend web' descripcion='Creating of responsives and moderns interfaces with technologies like React, HTML, CSS and JavaScript' />
+        <Service icono={<i className="bi bi-code-square"></i>} titulo='Frontend web' descripcion='Creating of responsives and moderns interfaces with technologies like React, HTML, CSS and JavaScript.' />
+        <Service icono={<i className="bx bx-server"></i>} titulo = 'Backend web' descripcion='Implementation of robust and secure systems with user authentication, APIs and databases.'/>
       </div>
     </section>
   )
