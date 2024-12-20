c60556897a4680319be8e27208c032ac3d2b7751
santimontironi <santiimontironi@gmail.com>
2024-12-20 19:40:14 -0300

    Se centro con sombreado el logo del servicio

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,11 +5,11 @@ export const Services = () => {
   return (
     <section id="services">
       <h1 className='titulo-services'>My services</h1>
-      <div className="contenedor-servicios">
+      <div className="container contenedor-servicios">
         <Service icono={<i className="bi bi-code-square"></i>} titulo='Frontend Web' descripcion='Creating of responsives and moderns interfaces with technologies like React, HTML, CSS, Bootstrap and JavaScript.' />
         <Service icono={<i className="bx bx-server"></i>} titulo = 'Backend Web' descripcion='Implementation of robust and secure systems with user authentication, APIs and databases.'/>
         <Service icono={<i className='bi bi-database'></i>} titulo = 'Databases' descripcion = 'Creation and Maintenance of efficient, secure, and scalable Databases with MySQL and MongoDB for robust data solutions'/>
-        <Service icono={<i className='bi bi-phone'></i>} titulo = 'Responsive Design' descripcion = 'Responsive design for mobile websites and apps for best user experiencie.'/>
+        <Service icono={<i className='bi bi-phone'></i>} titulo = 'Responsive Web' descripcion = 'Responsive design for mobile websites and apps for best user experiencie.'/>
       </div>
     </section>
   )
