f9dc9d662486a93188f9561a97d8757ddcaa1192
santimontironi <santiimontironi@gmail.com>
2025-01-06 16:33:53 -0300

    Modificacion de la interfaz de Services

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -18,7 +18,7 @@ export const AboutMe = () => {
 
           <p>Hi! I am <span>Santiago Montironi</span>, a web developer from Argentina, and I am 22 years old.</p>
 
-          <p>As the founder of <a href="https://fullwebportfolio.com/" target='blank_'>FullWeb</a>, a web development startup, I specialize in creating high-performance and customized web applications.</p>
+          <p>As the founder of <a href="https://fullwebportfolio.com/" target='blank_'>FullWeb</a> , a web development startup, I specialize in creating high-performance and customized web applications.</p>
 
           <p>I enjoy exploring new technologies and staying updated on the latest trends. I’m always seeking to improve and learn with every project.</p>
 
