0fe07cdf57e203185d4802a1453e10b6446156bd
santimontironi <santiimontironi@gmail.com>
2025-01-09 23:53:38 -0300

    se añaderon nuevas skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,6 +1,7 @@
 import '../assets/css/skills.css'
 import logoHtml from '../assets/img/html.png'
 import logoCss from '../assets/img/css.png'
+import wordpress from '../assets/img/wordpress.png'
 import logoPython from '../assets/img/python.png'
 import logoJs from '../assets/img/javascript.png'
 import cSharp from '../assets/img/cSharp.png'
@@ -8,24 +9,37 @@ import react from '../assets/img/react.png'
 import django from '../assets/img/django.png'
 import flask from '../assets/img/flask.png'
 import bootstrap from '../assets/img/bootstrap.png'
+import docker from '../assets/img/docker.png'
+import git from '../assets/img/git.png'
+import github from '../assets/img/github.png'
 import Wavify from "react-wavify";
 import { SkillCard } from '../components/SkillCard'
 
 export const Skills = () => {
 
   const technologiesAndLenguages = [
-    { logo: logoHtml, nombre: 'HTML' },
-    { logo: logoCss, nombre: 'CSS' },
     { logo: logoPython, nombre: 'Python' },
     { logo: logoJs, nombre: 'JavaScript' },
     { logo: cSharp, nombre: 'C#' },
   ];
 
+  const WebDesignAndLayout = [
+    { logo: logoHtml, nombre: 'HTML' },
+    { logo: logoCss, nombre: 'CSS' },
+    { logo: bootstrap, nombre: 'Bootstrap' },
+    { logo: wordpress, nombre: 'Wordpress' }
+  ]
+
   const frameworks = [
     { logo: react, nombre: 'React' },
     { logo: django, nombre: 'Django' },
     { logo: flask, nombre: 'Flask' },
-    { logo: bootstrap, nombre: 'Bootstrap' },
+  ]
+
+  const tools = [
+    {logo: docker, nombre: 'Docker'},
+    {logo: git, nombre: 'Git'},
+    {logo: github, nombre: 'GitHub'}
   ]
 
   return (
@@ -42,8 +56,10 @@ export const Skills = () => {
         }}
       />
       <div className="contenedor-skills">
-        <SkillCard categoriaSkill="Lenguajes y tecnologias" skills={technologiesAndLenguages} />
+        <SkillCard categoriaSkill="Web Design & Layout" skills={WebDesignAndLayout}/>
+        <SkillCard categoriaSkill="Languages" skills={technologiesAndLenguages} />
         <SkillCard categoriaSkill="Frameworks" skills={frameworks} />
+        <SkillCard categoriaSkill="Tools" skills={tools}/>
       </div>
     </section>
   )
