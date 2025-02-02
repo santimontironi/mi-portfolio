d39dd587dae30b5839927c9a4086a6b289fe695b
santimontironi <santiimontironi@gmail.com>
2025-02-01 23:29:46 -0300

    Formulario agregado

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,15 @@
+import '../assets/css/cssComponents/form.css'
+
 export const Form = () => {
   return (
-    <div>Form</div>
+    <div className="contenedor-form">
+        <h1 className='text-light border-light border-3 pb-4 border-bottom'>Get in touch</h1>
+        <form action="">
+            <input type="text" placeholder='Your name' />
+            <input type="text" placeholder='Your surname' />
+            <textarea placeholder='Typing your message here'></textarea>
+            <input type="submit" value="Send" />
+        </form>
+    </div>
   )
 }
