ff306b4c556cda993ba1793fde497018b2b397f2
santimontironi <santiimontironi@gmail.com>
2025-02-03 02:28:53 -0300

    Se agrego la api para mandar correos al mandar el formulario de contacto

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,10 +4,10 @@ export const Form = () => {
   return (
     <div className="contenedor-form">
         <h1 className='text-light border-light border-3 pb-4 border-bottom'>Get in touch</h1>
-        <form action="">
-            <input type="text" placeholder='Your name' />
-            <input type="text" placeholder='Your surname' />
-            <textarea placeholder='Typing your message here'></textarea>
+        <form action="https://formsubmit.co/santiimontironi@gmail.com" method='post'>
+            <input name='Nombre' type="text" placeholder='Your name' required/>
+            <input name='Apellido' type="text" placeholder='Your surname' required/>
+            <textarea name='Mensaje' placeholder='Typing your message here' required></textarea>
             <input type="submit" value="Send" />
         </form>
     </div>
