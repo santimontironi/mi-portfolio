cc5de4eef04d06660aa830f2161f8f34784a946a
santimontironi <santiimontironi@gmail.com>
2025-02-18 12:28:24 -0300

    Footer terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,16 @@ export const Footer = () => {
   return (
     <footer>
         <div className="contenido-footer">
-            <div className="logoFooter">
+            <div className="miInfo">
                 <h1>Santiago <span>Montironi</span></h1>
                 <img className='logoBlanco' src="/src/assets/img/logo3.png" alt="" />
                 <div className="enlaces-contacto">
-                    <ul>
-                        <li>
-                            <a href="https://github.com/santimontironi">
-                                <img src="https://img.icons8.com/?size=100&id=12599&format=png&color=ffffff" alt="logo-git" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.linkedin.com/in/santiago-montironi-649652215/">
-                                <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" alt="logo-linkedIn" />
-                            </a>
-                        </li>
-                    </ul>
+                    <a href="https://github.com/santimontironi">
+                        <img src="https://img.icons8.com/?size=100&id=12599&format=png&color=ffffff" alt="logo-git" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/santiago-montironi-649652215/">
+                        <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" alt="logo-linkedIn" />
+                    </a>
                 </div>
             </div>
             <div className="links-rapidos">
