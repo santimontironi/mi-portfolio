2cad5284e89ce2b79f29636401f58ce80a07a046
santimontironi <santiimontironi@gmail.com>
2025-10-16 22:10:06 -0300

    contexto de cambio de idioma agregado, falta la logica

---
+++ src/context/languageContext.jsx
@@ -0,0 +1,12 @@
+import { createContext, useState } from "react";
+
+export const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+
+    const [language, setLanguage] = useState('es');
+
+    return <LanguageContext.Provider value={{language,setLanguage}}>
+        {children}
+    </LanguageContext.Provider>;
+};
