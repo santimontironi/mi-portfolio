69012d19da7c449c2efa0541e859cc1b88d979a8
santimontironi <santiimontironi@gmail.com>
2025-10-17 14:29:24 -0300

    traduccion completada

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,41 @@
 import '../assets/css/cssComponents/footer.css'
 import logo from '/img/logo3.png'
+import { useContext } from 'react'
+import { LanguageContext } from '../context/languageContext'
 
 export const Footer = () => {
-  return (
-    <footer>
-        <div className="contenido-footer">
-            <div className="miInfo">
-                <h1>Santiago Montironi</h1>
-                <img className='logoBlanco' src={logo} alt="" />
-                <div className="enlaces-contacto">
-                    <a href="https://github.com/santimontironi">
-                        <img src="https://img.icons8.com/?size=100&id=12599&format=png&color=ffffff" alt="logo-git" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/santiago-montironi-649652215/">
-                        <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" alt="logo-linkedIn" />
-                    </a>
+
+    const { language } = useContext(LanguageContext)
+    
+    return (
+        <footer>
+            <div className="contenido-footer">
+                <div className="miInfo">
+                    <h1>Santiago Montironi</h1>
+                    <img className='logoBlanco' src={logo} alt="" />
+                    <div className="enlaces-contacto">
+                        <a href="https://github.com/santimontironi">
+                            <img src="https://img.icons8.com/?size=100&id=12599&format=png&color=ffffff" alt="logo-git" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/santiago-montironi-649652215/">
+                            <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" alt="logo-linkedIn" />
+                        </a>
+                    </div>
+                </div>
+                <div className="links-rapidos">
+                    <h1>{language === 'es' ? 'Links rápidos' : 'Quick links'}</h1>
+                    <ul>
+                        <li><a href="#home" className="nav-link hvr-bounce-to-bottom">{language === 'es' ? 'Inicio' : 'Home'}</a></li>
+                        <li><a href="#aboutme" className="nav-link hvr-bounce-to-bottom">{language === 'es' ? 'Sobre mi' : 'About me'}</a></li>
+                        <li><a href="#services" className="nav-link hvr-bounce-to-bottom">{language === 'es' ? 'Servicios' : 'Services'}</a></li>
+                        <li><a href="#projects" className="nav-link hvr-bounce-to-bottom">{language === 'es' ? 'Proyectos' : 'Projects'}</a></li>
+                        <li><a href="#contact" className="nav-link hvr-bounce-to-bottom">{language === 'es' ? 'Contacto' : 'Contact' }</a></li>
+                    </ul>
                 </div>
             </div>
-            <div className="links-rapidos">
-                <h1>Links rápidos</h1>
-                <ul>
-                    <li><a href="#home" className="nav-link hvr-bounce-to-bottom">Inicio</a></li>
-                    <li><a href="#aboutme" className="nav-link hvr-bounce-to-bottom">Sobre mi</a></li>
-                    <li><a href="#services" className="nav-link hvr-bounce-to-bottom">Servicios</a></li>
-                    <li><a href="#projects" className="nav-link hvr-bounce-to-bottom">Proyectos</a></li>
-                    <li><a href="#contact" className="nav-link hvr-bounce-to-bottom">Contacto</a></li>
-                </ul>
+            <div className="txt-copyright">
+                <p>{language === 'es' ? 'Desarrollado por Santiago Montironi' : 'Developed by Santiago Montironi'} <a href="https://github.com/santimontironi">Santiago Montironi</a></p>
             </div>
-        </div>
-        <div className="txt-copyright">
-            <p>Portfolio desarrollado por <a href="https://github.com/santimontironi">Santiago Montironi</a></p>
-        </div>
-    </footer>
-  )
+        </footer>
+    )
 }
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -13,8 +13,8 @@ export const AboutMe = () => {
 
         <div className="info" data-aos="zoom-in">
           {language === 'es' ?
-            <p>Hola! Yo soy <span>Santiago Montironi</span>, un desarrollador web de Argentina, y tengo 23 años.
-              Como fundador de <a href="https://fullwebportfolio.com/" target='blank_'>FullWeb</a>, mi emprendimiento de desarrollo web, me especializo en crear aplicaciones web personalizadas y de alto rendimiento. Estoy dispuesto a trabajar en proyectos desafiantes y en equipo. Siéntete libre de contactarme para discutir tu proyecto.
+            <p>¡Hola! Soy <span>Santiago Montironi</span>, un desarrollador web de Argentina de 23 años.
+              Como fundador de <a href="https://fullwebpag.netlify.app/" target="_blank">FullWeb</a>, mi emprendimiento de desarrollo web, me dedico a crear aplicaciones web a medida y de alto rendimiento. Me encanta trabajar en proyectos desafiantes y en equipo. No dudes en contactarme si quieres hablar sobre tu proyecto
             </p>
             :
             <p>H1! I'm <span>Santiago Montironi</span>, a web developer from Argentina, and I'm 23 years old. As founder of <a href="https://fullwebportfolio.com/" target='blank_'>FullWeb</a>, my web development startup, I specialize in create high performance and personalized applications web. I'm open to work in challenging projects and in workteam. Feel free to contact to me for discuss your project.</p>}
