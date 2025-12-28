a242afb99c67457b6291461c7fb087001a918514
santimontironi <santiimontironi@gmail.com>
2025-12-27 22:10:26 -0300

    mejora de estilos en Project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -92,16 +92,6 @@ const projectsData = (language) => {
         urlDespliegue: 'https://wordlegamebysm.netlify.app/',
         iconoDespliegue: 'https://cdn.freebiesupply.com/logos/large/2x/netlify-logo-png-transparent.png',
         nombreEnlaceDespliegue: "Netlify"
-      },
-      {
-        fotoProyecto: imgProyectoCrudDjango,
-        tituloProyecto: "CRUD de tareas",
-        descripcionProyecto: "Aplicación que consiste en un CRUD de tareas, donde puedes registrarte o iniciar sesión y luego agregar, editar, marcar como completadas y ver todas las tareas. En este proyecto utilicé Django y base de datos SQL, y el diseño con HTML, CSS y Bootstrap.",
-        urlGitHub: "https://github.com/santimontironi/CRUD-Django",
-        iconoGitHub: "https://img.icons8.com/?size=100&id=12599&format=png&color=ffffff",
-        urlDespliegue: 'https://mysite-9s4w.onrender.com/',
-        iconoDespliegue: 'https://pbs.twimg.com/profile_images/1735429515541938176/zOO1N7Su_400x400.jpg',
-        nombreEnlaceDespliegue: 'Render'
       }
     ]
 
@@ -188,16 +178,6 @@ const projectsData = (language) => {
         urlDespliegue: 'https://wordlegamebysm.netlify.app/',
         iconoDespliegue: 'https://cdn.freebiesupply.com/logos/large/2x/netlify-logo-png-transparent.png',
         nombreEnlaceDespliegue: "Netlify"
-      },
-      {
-        fotoProyecto: imgProyectoCrudDjango,
-        tituloProyecto: "Task CRUD App",
-        descripcionProyecto: "A CRUD app where you can register or log in, and then add, edit, mark as completed, and view all your tasks. Built with Django, SQL database, and styled using HTML, CSS, and Bootstrap.",
-        urlGitHub: "https://github.com/santimontironi/CRUD-Django",
-        iconoGitHub: "https://img.icons8.com/?size=100&id=12599&format=png&color=ffffff",
-        urlDespliegue: 'https://mysite-9s4w.onrender.com/',
-        iconoDespliegue: 'https://pbs.twimg.com/profile_images/1735429515541938176/zOO1N7Su_400x400.jpg',
-        nombreEnlaceDespliegue: 'Render'
       }
     ]
   }
